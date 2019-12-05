561b50ce9c1d43f9687281970975f13d92fcab32
Captaincrogs <2003829@talnet.nl>
2019-12-05 11:14:40 +0100

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    for($i=0;$i<=9;$i++){
+
+    for ($d=10-$i; $d > 0; $d--)  {
+
+        echo "&nbsp;&nbsp;";
+    }
+    for($j=1;$j<=$i;$j++){
+
+        echo "&nbsp;".$i."&nbsp;";
+    }
+
+    echo "<br>";
+
+}
+
+?>
+  </body>
+</html>
+
+
+<?php
+
+
+  ?>
